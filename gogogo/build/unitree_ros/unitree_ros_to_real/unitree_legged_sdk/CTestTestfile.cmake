169b6c30ff90859150a5b44074372a40ab591b0c
Elwatkinn <2734847727@qq.com>
2025-03-12 11:26:17 +0800

    msg

--- gogogo/build/unitree_ros/unitree_ros_to_real/unitree_legged_sdk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neuro_1/gogogo/src/unitree_ros/unitree_ros_to_real/unitree_legged_sdk
-# Build directory: /home/neuro_1/gogogo/build/unitree_ros/unitree_ros_to_real/unitree_legged_sdk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
