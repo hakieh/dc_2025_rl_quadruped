0a9e13009a328b08b0d4f4da35f3fa9e926086bf
Elwatkinn <2734847727@qq.com>
2025-03-12 11:11:19 +0800

    msg

--- gogogo/build/unitree_guide/unitree_actuator_sdk/unitree_motor_ctrl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neuro_1/gogogo/src/unitree_guide/unitree_actuator_sdk/unitree_motor_ctrl
-# Build directory: /home/neuro_1/gogogo/build/unitree_guide/unitree_actuator_sdk/unitree_motor_ctrl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- gogogo/build/unitree_legged_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neuro_1/gogogo/src/unitree_legged_msgs
-# Build directory: /home/neuro_1/gogogo/build/unitree_legged_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
